dc7d8b71f6890f891f3e19858e9c3fa2f8e4e35a
Nico_nieee <145033003+Bun-nie@users.noreply.github.com>
2023-12-08 15:20:50 +0800

    Update AccountNumberValidator.java

--- Facade Design Pattern/src/BankAccount/AccountNumberValidator.java
+++ Facade Design Pattern/src/BankAccount/AccountNumberValidator.java
@@ -1,15 +1,6 @@
 package BankAccount;
-
 public class AccountNumberValidator {
     private final int accountNumber = 123456789;
-
-    public AccountNumberValidator() {
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
     public boolean validateAccount(int account){
         return account == accountNumber;
     }
