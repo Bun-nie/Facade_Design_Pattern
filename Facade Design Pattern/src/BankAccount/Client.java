22adc39e695df67a34b1a4813a2f46b1dbe9bf2f
Nico_nieee <145033003+Bun-nie@users.noreply.github.com>
2023-12-08 15:19:23 +0800

    Update Client.java

--- Facade Design Pattern/src/BankAccount/Client.java
+++ Facade Design Pattern/src/BankAccount/Client.java
@@ -1,10 +1,7 @@
 package BankAccount;
-
 public class Client {
     public static void main(String[] args) {
-        Display display = new Display();
-        display.welcome();
-        BankAccountFacade facade = new BankAccountFacade(display.askAccountNumber(), display.askPIN());
+        BankAccountFacade facade = new BankAccountFacade();
         facade.doTransaction();
     }
 }
