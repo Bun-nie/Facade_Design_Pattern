2c3684fc990dd246b2062a76453abf400a94362f
Nico_nieee <145033003+Bun-nie@users.noreply.github.com>
2023-12-08 15:22:02 +0800

    Update PINValidator.java

--- Facade Design Pattern/src/BankAccount/PINValidator.java
+++ Facade Design Pattern/src/BankAccount/PINValidator.java
@@ -1,11 +1,6 @@
 package BankAccount;
-
 public class PINValidator {
     private final int PIN = 1234;
-
-    public PINValidator() {
-    }
-
     public boolean validatePin(int pin){
         return pin == PIN;
     }
